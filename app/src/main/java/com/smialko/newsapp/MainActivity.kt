99c1f47629ca6447192925526481639567b58b17
vova77 <vova2000smi@gmail.com>
2024-06-29 22:08:11 +0300

    add NavGraph

--- app/src/main/java/com/smialko/newsapp/MainActivity.kt
+++ app/src/main/java/com/smialko/newsapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,7 +20,9 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.smialko.newsapp.domain.usecases.AppEntryUseCases
+import com.smialko.newsapp.presentation.nvgraph.NavGraph
 import com.smialko.newsapp.presentation.onboarding.OnBoardingScreen
 import com.smialko.newsapp.presentation.onboarding.OnBoardingViewModel
 import com.smialko.newsapp.ui.theme.NewsAppTheme
@@ -30,27 +33,22 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @Inject
-    lateinit var useCases: AppEntryUseCases
+    val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        installSplashScreen()
-
-        lifecycleScope.launch {
-            useCases.readAppEntry().collect {
-                Log.d("TEST", it.toString())
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.splashCondition
             }
         }
         enableEdgeToEdge()
         setContent {
             NewsAppTheme {
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
-                    val viewModel: OnBoardingViewModel = hiltViewModel()
-                    OnBoardingScreen(
-                        event = viewModel::onEvent
-                    )
+                    val startDestination = viewModel.startDestination
+                    NavGraph(startDestination = startDestination)
                 }
             }
         }
